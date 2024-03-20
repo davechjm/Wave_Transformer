d0379613e9d88c2d04c77f5e329c6feb710cb420
choi <choi@master.ismll.de>
2024-03-20 10:30:12 +0100

    update

--- optuna_/bash.sh
+++ optuna_/bash.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=wave_t_omulti
+#SBATCH --job-name=wave_t_o
 #SBATCH --output=py_torch_test%j.log
 #SBATCH --error=py_torch_test%j.err
 #SBATCH --mail-user=choi@uni-hildesheim.de
@@ -10,4 +10,4 @@ cd python /home/choi/Wave_Transformer/optuna_     # navigate to the directory if
 
 source activate pytorchenv
 #srun python vanila_transformer_dwt_.py        # python jobs require the srun command to work
-srun python DUTC_Test_RevIN_Multi.py
+srun python DUTC_Test_RevIN.py
